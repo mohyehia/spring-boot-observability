9ed5608d8c069d181f973d8bf9fe494c89a6d912
mohyehia <mohammedyehia99@gmail.com>
2023-10-02 19:53:37 +0300

    Initial Commit

---
+++ src/main/java/com/moh/yehia/observability/SpringBootObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.moh.yehia.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootObservabilityApplication.class, args);
+	}
+
+}
