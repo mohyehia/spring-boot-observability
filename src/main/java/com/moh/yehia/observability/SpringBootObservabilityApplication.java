de42641b998deb9311b161e3b07c2628e66e426a
mohyehia <mohammedyehia99@gmail.com>
2023-10-06 16:06:47 +0300

    Notes: Adding distributed tracing This commit adds distributed tracing using micrometer & openTelemetry dependencies & exports traces to zipkin server for visualization

--- src/main/java/com/moh/yehia/observability/SpringBootObservabilityApplication.java
+++ src/main/java/com/moh/yehia/observability/SpringBootObservabilityApplication.java
@@ -2,12 +2,20 @@ package com.moh.yehia.observability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootObservabilityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootObservabilityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootObservabilityApplication.class, args);
+    }
 
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
 }
--- src/main/java/com/moh/yehia/observability/controller/ProductController.java
+++ src/main/java/com/moh/yehia/observability/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.moh.yehia.observability.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +15,10 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/products")
 @Log4j2
+@RequiredArgsConstructor
 public class ProductController {
+    private final RestTemplate restTemplate;
+
     @GetMapping
     public List<Product> findAll() {
         log.info("ProductController :: findAll :: start");
@@ -21,6 +27,12 @@ public class ProductController {
                 new Product(UUID.randomUUID().toString(), "Second Random Product", 10),
                 new Product(UUID.randomUUID().toString(), "Last Product Name", 15)
         );
+    }
+
+    @GetMapping("/trace")
+    public String trace() {
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://httpbin.org/post", "Hello, Actuator", String.class);
+        return responseEntity.getBody();
     }
 }
 
