a4b1460c39192e0d84d97d67524822bfa155cb43
mohyehia <mohammedyehia99@gmail.com>
2024-08-22 12:35:29 +0300

    Adding `open-api` configuration for generating api-docs and swagger-ui

---
+++ src/main/java/com/moh/yehia/observability/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.moh.yehia.observability.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Spring Boot Observability API")
+                        .description("Spring Boot Observability API Documentation")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to this Wiki for more documentation")
+                        .url("/api-docs"));
+    }
+}
